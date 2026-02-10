408fefbf0e9187ec8e20ab4321c2d09ee1a77458
Vishal45t <talawarvishal3@gmail.com>
2026-02-10 22:22:25 +0530

    initial commit

--- app.js
+++ app.js
@@ -1,117 +1,100 @@
+require("dotenv").config();
+
 const express = require("express");
 const app = express();
 const mongoose = require("mongoose");
-const Listing = require("./models/listing.js");
 const path = require("path");
 const ejsMate = require("ejs-mate");
-const wrapAsync = require("./utils/wrapAsync.js");
-const ExpressError = require("./utils/ExpressError.js");
-const {listingSchema,reviewSchema} = require("./schema.js");
-const Review = require("./models/review.js");
-const session = require("express-session");
-const flash = require("connect-flash");
-const passport = require("passport");
-const localStrategy = require("passport-local");
-const User = require("./models/user.js");
-const MongoStore = require("connect-mongo");
-
 const methodOverride = require("method-override");
 
-app.use(methodOverride("_method"));
+const ExpressError = require("./utils/ExpressError.js");
 
+const session = require("express-session");
+const MongoStore = require("connect-mongo");
+const flash = require("connect-flash");
 
-app.use(express.urlencoded({ extended: true }));  // for form data
-app.use(express.json());  // for JSON data
+const passport = require("passport");
+const LocalStrategy = require("passport-local");
+const User = require("./models/user.js");
 
-
+// ------------------- Routers -------------------
 const listingRouter = require("./route/listing.js");
 const reviewRouter = require("./route/reviews.js");
 const userRouter = require("./route/user.js");
 
-// const MONGO_URL = "mongodb://127.0.0.1:27017/wanderlust";
+// ------------------- App Config -------------------
+app.set("view engine", "ejs");
+app.set("views", path.join(__dirname, "views"));
+app.engine("ejs", ejsMate);
 
-const dbUrl =  process.env.AtlasDB;
+app.use(express.urlencoded({ extended: true }));
+app.use(express.json());
+app.use(methodOverride("_method"));
+app.use(express.static(path.join(__dirname, "public")));
 
+// ------------------- Database -------------------
+const dbUrl = process.env.AtlasDB;
 
-main()
-  .then(() => {
-    console.log("Connected to DB");
-  })
-  .catch((err) => {
-    console.log(err);
-  });
+mongoose
+  .connect(dbUrl)
+  .then(() => console.log("Connected to DB"))
+  .catch((err) => console.log(err));
 
-async function main() {
-  await mongoose.connect(dbUrl);
-}
-
+// ------------------- Session Store -------------------
 const store = MongoStore.create({
-    mongoUrl: dbUrl,
-    crypto: {
-        secret: process.env.SECRET,
-    },
-    touchAfter: 24 * 3600,
+  mongoUrl: dbUrl,
+  crypto: {
+    secret: process.env.SECRET,
+  },
+  touchAfter: 24 * 3600,
 });
 
 const sessionOptions = {
-    store,
-    secret: process.env.SECRET,
-    resave: false,
-    saveUninitialized: true,
-    cookie: {
-        expires: Date.now() + 7 * 24 * 60 * 60 * 1000,
-        maxAge: 7 * 24 * 60 * 60 * 1000,
-        httpOnly: true,
-    }
+  store,
+  secret: process.env.SECRET,
+  resave: false,
+  saveUninitialized: false,
+  cookie: {
+    httpOnly: true,
+    expires: Date.now() + 7 * 24 * 60 * 60 * 1000,
+    maxAge: 7 * 24 * 60 * 60 * 1000,
+  },
 };
-
-
-// app.get("/", (req, res) => {
-//   res.send("hi, I am root");
-// });
 
 app.use(session(sessionOptions));
 app.use(flash());
 
+// ------------------- Passport -------------------
 app.use(passport.initialize());
 app.use(passport.session());
-passport.use(new localStrategy(User.authenticate()));
-
+passport.use(new LocalStrategy(User.authenticate()));
 passport.serializeUser(User.serializeUser());
 passport.deserializeUser(User.deserializeUser());
 
+// ------------------- Flash Middleware -------------------
 app.use((req, res, next) => {
-    res.locals.success = req.flash("success");
-     res.locals.error = req.flash("error");
-     res.locals.currUser = req.user;               //because ejs cannot directly access req.user
-    next();
+  res.locals.success = req.flash("success");
+  res.locals.error = req.flash("error");
+  res.locals.currUser = req.user;
+  next();
 });
 
 // ------------------- Routes -------------------
-
 app.use("/listings", listingRouter);
 app.use("/listings/:id/reviews", reviewRouter);
-app.use("/",userRouter);
-
-// ------------------- Middlewares -------------------
-app.set("view engine", "ejs");
-app.set("views", path.join(__dirname, "views"));
-app.engine("ejs", ejsMate);
-app.use(express.urlencoded({ extended: true }));
-app.use(methodOverride("_method"));
-app.use(express.static(path.join(__dirname, "/public")));
+app.use("/", userRouter);
 
 // ------------------- Error Handling -------------------
 app.use((req, res, next) => {
-  next(new ExpressError(404, "Page Not Found!"));         //For all get request expect defined
+  next(new ExpressError(404, "Page Not Found!"));
 });
 
 app.use((err, req, res, next) => {
   const { statusCode = 500, message = "Something went wrong" } = err;
-  res.render("listings/error.ejs", {message});
+  res.status(statusCode).render("listings/error.ejs", { message });
 });
 
 // ------------------- Server -------------------
 app.listen(8080, () => {
-  console.log("Server is listening on port 8080");
+  console.log("Server running on port 8080");
 });
--- controllers/listings.js
+++ controllers/listings.js
@@ -1,76 +1,65 @@
-const Listing = require("../models/listing");
+const Listing = require("../models/listing.js");
 
+// 👉 INDEX + SEARCH
 module.exports.index = async (req, res) => {
-  const allListings = await Listing.find({});
-  res.render("listings/index.ejs", { allListings });
-};
+  const { q } = req.query;
 
-module.exports.renderNewForm = (req, res) => {
-  res.render("listings/new.ejs");
-};
+  let listings;
 
-module.exports.showListing =async (req, res) => {
-  const { id } = req.params;
-const listing = await Listing.findById(req.params.id)
-  .populate("owner")
-  .populate({
-    path: "reviews",
-    populate: { path: "author",model: "User" }
-});
-    if(!listing){
-        req.flash("error","Listing not existed!");
-        res.redirect("/listings")
-      }
-      res.render("listings/show.ejs", { listing });
-};
-
-module.exports.createListing = async (req, res) => {
-    //     if(!req.body.listing){
-    //     throw new ExpressError(400,"Enter Valid listing");
-    // }
-    const { title, description, image, price, location, country } = req.body;
-    const newListing = new Listing({
-      title,
-      description,
-      image,
-      price,
-      location,
-      country,
+  if (q) {
+    listings = await Listing.find({
+      $or: [
+        { title: { $regex: q, $options: "i" } },
+        { location: { $regex: q, $options: "i" } }
+      ],
     });
-    newListing.owner = req.user._id;
-    await newListing.save();
-    req.flash("success","Listing created successfulyy!");
-    res.redirect("/listings");
-  };
-
-module.exports.editListing = async (req, res) => {
-  const { id } = req.params;
-  const listing = await Listing.findById(id);
-    if(!listing){
-    req.flash("error","Listing not existed!");
-    res.redirect("/listings")
+  } else {
+    listings = await Listing.find({});
   }
-  res.render("listings/edit.ejs", { listing });
-}; 
+    res.render("listings/index", { listings });
 
-module.exports.updateListing = async (req, res) => {
-            if(!req.body.listing){
-        throw new ExpressError(400,"Enter Valid listing");
-    }
+};
+
+// 👉 NEW
+module.exports.renderNewForm = (req, res) => {
+  res.render("listings/new");
+};
+
+// 👉 SHOW
+module.exports.showListing = async (req, res) => {
   const { id } = req.params;
-  const { title: newTitle } = req.body;
-  await Listing.findByIdAndUpdate(
-    id,
-    { title: newTitle },
-    { runValidators: true, new: true }
-  );
-  req.flash("success","Listing updated successfulyy!");
+  const listing = await Listing.findById(id).populate("reviews").populate("owner");
+  res.render("listings/show", { listing });
+};
+
+// 👉 CREATE
+module.exports.createListing = async (req, res) => {
+  const listing = new Listing(req.body.listing);
+  listing.owner = req.user._id;
+  await listing.save();
+  req.flash("success", "New listing created!");
   res.redirect("/listings");
 };
 
+// 👉 EDIT
+module.exports.editListing = async (req, res) => {
+  const { id } = req.params;
+  const listing = await Listing.findById(id);
+  res.render("listings/edit", { listing });
+};
+
+// 👉 UPDATE
+module.exports.updateListing = async (req, res) => {
+  const { id } = req.params;
+  await Listing.findByIdAndUpdate(id, { ...req.body.listing });
+  req.flash("success", "Listing updated!");
+  res.redirect(`/listings/${id}`);
+};
+
+// 👉 DELETE
 module.exports.destroyListing = async (req, res) => {
   const { id } = req.params;
   await Listing.findByIdAndDelete(id);
-  req.flash("success","Listing deleted successfully");
+  req.flash("success", "Listing deleted!");
   res.redirect("/listings");
 };
